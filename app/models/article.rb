36adb19aa4d266168124a21d171a6e251047af9e
佐藤良昭 <yoshiprograming@gmail.com>
2020-03-22 14:11:25 +0900

    140もじに変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,3 @@
 class Article < ApplicationRecord
-    validates :content,    length: { in: 1..75 }        # 「1文字以上75文字以下」
+    validates :content,    length: { in: 1..140 }        # 「1文字以上75文字以下」
 end
